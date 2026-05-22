26a446e596a0f8f6ae1f62db28dac41659581f31
tanle <tan.le@kyanon.digital>
2026-05-22 15:58:50 +0700

    fix: CORS allow origin patterns instead of origins

--- src/main/java/com/autolift/config/SecurityConfig.java
+++ src/main/java/com/autolift/config/SecurityConfig.java
@@ -54,10 +54,11 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("*"));
+    configuration.setAllowedOriginPatterns(List.of("*"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setExposedHeaders(List.of("Authorization"));
+    configuration.setAllowCredentials(false);
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
     return source;
